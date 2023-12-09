fc965c6735edf9043b0f1eb8308395e1a4c3b0f7
Zhadi <zhadi000s@gmail.com>
2023-12-09 13:44:57 +0600

    Закончил основные стили в компоненте клубы

--- BronClub/src/app/services/club.service.ts
+++ BronClub/src/app/services/club.service.ts
@@ -9,7 +9,8 @@ import { Club } from '../models/club.model';
 export class ClubService {
 
   clubs:Club[] = [
-    {id:1,name:'Bro',rating:5.0,mainPhoto:'assets/clubs/Bro/main.png',photoURL:['assets/clubs/Bro/area.jpg','assets/clubs/Bro/area2.jpg'],location:'Abaya 55'}
+    {id:1,name:'Bro',rating:5.0,mainPhoto:'assets/clubs/Bro/main.png',photoURL:['assets/clubs/Bro/area.jpg','assets/clubs/Bro/area2.jpg'],location:'Abaya 55'},
+    {id:2,name:'Dofg',rating:6.8,mainPhoto:'assets/clubs/dofg/main.jpg',photoURL:['some photos'],location:'Тимирязова 132а'}
   ];
 
   constructor() { }
