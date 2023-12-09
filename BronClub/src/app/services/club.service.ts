3d8eb2551301efd3bff95bfc52a523be95957d7e
Zhadi <zhadi000s@gmail.com>
2023-12-10 00:57:09 +0600

    добавляю детали

--- BronClub/src/app/services/club.service.ts
+++ BronClub/src/app/services/club.service.ts
@@ -10,7 +10,8 @@ export class ClubService {
 
   clubs:Club[] = [
     {id:1,name:'Bro',rating:5.0,mainPhoto:'assets/clubs/Bro/main.png',photoURL:['assets/clubs/Bro/area.jpg','assets/clubs/Bro/area2.jpg'],location:'Abaya 55'},
-    {id:2,name:'Dofg',rating:6.8,mainPhoto:'assets/clubs/dofg/main.jpg',photoURL:['some photos'],location:'Тимирязова 132а'}
+    {id:2,name:'Dofg',rating:6.8,mainPhoto:'assets/clubs/dofg/main.jpg',photoURL:['some photos'],location:'Тимирязова 132а'},
+    {id:3,name:'Chicago',rating:10, mainPhoto:'assets/clubs/chicago/main.png',photoURL:['somPhoto'],location:'Тоганас Батыр 102'}
   ];
 
   constructor() { }
