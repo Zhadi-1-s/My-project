50dae3e139dca05f6ffbe55388f3f7d64cc91a86
Zhadi <zhadi000s@gmail.com>
2024-01-13 22:44:42 +0600

    внедрил слайд способ просотмра картинок в клуб компонент

--- BronClub/src/app/components/club/presentation/club.component.ts
+++ BronClub/src/app/components/club/presentation/club.component.ts
@@ -37,4 +37,22 @@ export class ClubComponent implements OnInit {
     this.selectedIndex = index
   }
 
+  onPrevClick():void{
+    if(this.selectedIndex === 0){
+      this.selectedIndex = this.images.length - 1
+    }
+    else{
+      this.selectedIndex--
+    }
+  }
+
+  onNextClick():void{
+      if(this.selectedIndex === this.images.length -1){
+        this.selectedIndex = 0;
+      }
+      else{
+        this.selectedIndex++;
+      }
+  }
+
 }
